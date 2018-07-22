1d90e7e548e829ec98d26bd6ca514db17d1e297d
Kumar Harsh <khs@playlyfe.com>
2018-07-22 20:58:34 +0530

    chore(test): fix test config ‍🤦‍♂️

--- karma.conf-ci.js
+++ karma.conf-ci.js
@@ -1,105 +1,97 @@
+// Browsers to run on Sauce Labs
+var customLaunchers = {
+  // IEs
+  SL_IE11_Win10: {
+    base: 'SauceLabs',
+    browserName: 'internet explorer',
+    version: '11.0',
+    platform: 'Windows 10'
+  },
+  SL_IE11_Win7: {
+    base: 'SauceLabs',
+    browserName: 'internet explorer',
+    version: '11.0',
+    platform: 'Windows 7'
+  },
+  SL_IE10_Win7: {
+    base: 'SauceLabs',
+    browserName: 'internet explorer',
+    version: '10.0',
+    platform: 'Windows 7'
+  },
+  // SL_IE9_Win7: {
+  //   base: 'SauceLabs',
+  //   browserName: 'internet explorer',
+  //   version: '9.0',
+  //   platform: 'Windows 7'
+  // },
+
+  // all other latest browsers
+  SL_Chrome_Win10: {
+    base: 'SauceLabs',
+    browserName: 'chrome',
+    version: 'latest',
+    platform: 'Windows 10'
+  },
+  SL_Chrome_Linux: {
+    base: 'SauceLabs',
+    browserName: 'chrome',
+    version: 'latest',
+    platform: 'Linux'
+  },
+  SL_Firefox_Win10: {
+    base: 'SauceLabs',
+    browserName: 'firefox',
+    version: 'latest',
+    platform: 'Windows 10'
+  },
+  SL_Firefox_Linux: {
+    base: 'SauceLabs',
+    browserName: 'firefox',
+    version: 'latest',
+    platform: 'Linux'
+  },
+  SL_Edge: {
+    base: 'SauceLabs',
+    browserName: 'microsoftedge',
+    version: 'latest',
+    platform: 'Windows 10'
+  },
+  SL_Safari_OSX: {
+    base: 'SauceLabs',
+    browserName: 'safari',
+    platform: 'macOS 10.12',
+    version: '11.0'
+  }
+};
+
 module.exports = function(config) {
-  // Browsers to run on Sauce Labs
-  var customLaunchers = {
-    // IEs
-    IE11_Win10: {
-      base: 'SauceLabs',
-      browserName: 'internet explorer',
-      version: '11.0',
-      platform: 'Windows 10'
-    },
-    IE11_Win7: {
-      base: 'SauceLabs',
-      browserName: 'internet explorer',
-      version: '11.0',
-      platform: 'Windows 7'
-    },
-    IE10_Win7: {
-      base: 'SauceLabs',
-      browserName: 'internet explorer',
-      version: '10.0',
-      platform: 'Windows 7'
-    },
-    // IE9_Win7: {
-    //   base: 'SauceLabs',
-    //   browserName: 'internet explorer',
-    //   version: '9.0',
-    //   platform: 'Windows 7'
-    // },
-
-    // all other latest browsers
-    Chrome_Win10: {
-      base: 'SauceLabs',
-      browserName: 'chrome',
-      version: 'latest',
-      platform: 'Windows 10'
-    },
-    Chrome_Linux: {
-      base: 'SauceLabs',
-      browserName: 'chrome',
-      version: 'latest',
-      platform: 'Linux'
-    },
-    Firefox_Win10: {
-      base: 'SauceLabs',
-      browserName: 'firefox',
-      version: 'latest',
-      platform: 'Windows 10'
-    },
-    Firefox_Linux: {
-      base: 'SauceLabs',
-      browserName: 'firefox',
-      version: 'latest',
-      platform: 'Linux'
-    },
-    Edge: {
-      base: 'SauceLabs',
-      browserName: 'MicrosoftEdge',
-      version: '17.17134',
-      platform: 'Windows 10'
-    },
-    Safari_OSX: {
-      base: 'SauceLabs',
-      browserName: 'safari',
-      platform: 'macOS 10.13',
-      version: '11.1'
-    }
-  };
-
   config.set({
-    // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
-
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine'],
-
-    // list of files / patterns to load in the browser
     files: ['polyfill.js', 'tests/*.test.js'],
-
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['dots', 'saucelabs'],
-
-    // web server port
     port: 9876,
-
     colors: true,
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
 
-    sauceLabs: {
-      testName: 'CustomEvent Polyfill'
-    },
-    captureTimeout: 120000,
-    customLaunchers: customLaunchers,
-
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: Object.keys(customLaunchers),
-    singleRun: true
+    customLaunchers: customLaunchers,
+    singleRun: true,
+    reporters: ['dots', 'saucelabs'],
+    plugins: ['karma-jasmine', 'karma-sauce-launcher'],
+
+    // SauceLabs allows for 2 tunnels only, therefore some browsers will have to wait
+    // rather long time. Plus mobile emulators tend to require a lot of time to start up.
+    // 10 minutes should be more than enough to run all of them.
+    browserNoActivityTimeout: 600000,
+    captureTimeout: 600000,
+    sauceLabs: {
+      startConnect: false,
+      tunnelIdentifier: process.env.TRAVIS_JOB_NUMBER,
+      recordScreenshots: false,
+      recordVideo: false,
+      testName: 'CustomEvent Polyfill',
+      public: 'public'
+    },
+    frameworks: ['jasmine']
   });
 };
